ddc3ad6707615b36d424d7224b7aaca7f9f452cb
lmajano07 <lmajano0701@gmail.com>
2024-10-15 21:59:56 -0600

    chore: added mapper functions

--- lib/features/transactions/infrastructure/mappers/transaction_mapper.dart
+++ lib/features/transactions/infrastructure/mappers/transaction_mapper.dart
@@ -1,1 +1,25 @@
-class TransactionMapper {}
+import 'package:transactions_app/features/transactions/domain/domain.dart';
+
+import 'package:transactions_app/features/shared/shared.dart';
+
+class TransactionMapper {
+  TransactionMapper._();
+
+  static Transaction mapToEntity(Map<String, dynamic> map) {
+    return Transaction(
+      amount: map['amount'],
+      description: map['description'],
+      type: transactionTypeFromString(map['type']),
+      createdAt: DateTime.parse(map['createdAt']),
+    );
+  }
+
+  static Map<String, dynamic> entityToMap(Transaction transaction) {
+    return {
+      'amount': transaction.amount,
+      'description': transaction.description,
+      'type': transaction.type.toString(),
+      'createdAt': transaction.createdAt,
+    };
+  }
+}
